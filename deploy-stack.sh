21e6c7044c2037ab2a3432361cf1728040efa5c7
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-11 18:09:32 +0530

    Update deploy-stack.sh

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -1,1 +1,10 @@
-
+if [ $# -ne 5 ]; then
+    echo "Enter stack name, parameters file name, template file name to create, set changeset value (true or false), and enter region name. "
+    exit 0
+else
+    STACK_NAME=$1
+    PARAMETERS_FILE_NAME=$2
+    TEMPLATE_NAME=$3
+    CHANGESET_MODE=$4
+    REGION=$5
+fi

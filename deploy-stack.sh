8ceabd4b77ac4ae7aa0dba688f023edfdf96e3c9
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-11 18:32:00 +0530

    Update deploy-stack.sh

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -1,10 +1,19 @@
-if [ $# -ne 5 ]; then
-    echo "Enter stack name, parameters file name, template file name to create, set changeset value (true or false), and enter region name. "
+if [ $# -ne 4 ]; then
+    echo "Enter stack name, parameters file name, template file name to create and enter region name. "
     exit 0
 else
     STACK_NAME=$1
     PARAMETERS_FILE_NAME=$2
     TEMPLATE_NAME=$3
-    CHANGESET_MODE=$4
-    REGION=$5
+    REGION=$4
+fi
+
+if [[ $TEMPLATE_NAME != *.yaml ]]; then
+    echo "CloudFormation template $TEMPLATE_NAME does not exist. Make sure the extension is *.yaml and not (*.yml)"
+    exit 0
+fi
+
+if [[ $PARAMETERS_FILE_NAME != *.properties ]]; then
+    echo "CloudFormation parameters $PARAMETERS_FILE_NAME does not exist"
+    exit 0
 fi

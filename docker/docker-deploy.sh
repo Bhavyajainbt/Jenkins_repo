a7784fc762c0c3311d8b7129eb2e01dc3fd3613a
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-15 16:02:39 +0530

    Update docker-deploy.sh

--- docker/docker-deploy.sh
+++ docker/docker-deploy.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Enter stack name, template file name to create and region name. "
-    exit 0
-else
-    STACK_NAME=$1
-    TEMPLATE_NAME=$2
-    REGION=$3
-fi
-
-if [[ $TEMPLATE_NAME != *.yaml ]]; then
-    echo "CloudFormation template $TEMPLATE_NAME does not exist. Make sure the extension is *.yaml and not (*.yml)"
-    exit 0
-fi
-
-aws cloudformation deploy \
-  --stack-name $STACK_NAME \
-  --template-file template/${TEMPLATE_NAME} \
-  --region $REGION \
+docker build -t http-image .

d3b2b811992e698d5775a5547bf3778056facc09
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-15 16:11:43 +0530

    Update docker-deploy.sh

--- docker/docker-deploy.sh
+++ docker/docker-deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-IMAGE_NAME = $1
-docker build -t $IMAGE_NAME .
+if [ $# -ne 1 ]; then
+  echo "Type the Name of docker Image"
+  exit 0
+else
+  IMAGE_NAME = $1
+  docker build -t $IMAGE_NAME .
+fi

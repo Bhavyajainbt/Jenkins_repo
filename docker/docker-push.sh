07f39da23b24bf54f44298ca2b73d259c94bb903
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-15 17:52:42 +0530

    Update docker-push.sh

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+if [ $# -ne 2 ];then
+  echo "Please enter both Image name and Image tag name"
+  exit 0
+else
+  IMAGE_REPO_NAME=$1
+  IMAGE_TAG=$2
+  REPOSITORY_URI="public.ecr.aws/f5k7w3r3/bhavya-repo"
+fi
+
+docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG

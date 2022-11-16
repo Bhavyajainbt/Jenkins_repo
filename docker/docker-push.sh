12900e38381321f3c20281ff3a78ecb70f36ab63
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-16 20:32:41 +0530

    Update docker-push.sh

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 2 ];then
+if [ $# -ne 3 ];then
   echo "Please enter both Image name and Image tag name"
   exit 0
 else
   IMAGE_REPO_NAME=$1
   IMAGE_TAG=$2
-  REPOSITORY_URI="190698788805.dkr.ecr.us-east-1.amazonaws.com/bhavya-repo"
+  REPOSITORY_URI=$3
 fi
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 190698788805.dkr.ecr.us-east-1.amazonaws.com
-docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG
+docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:${IMAGE_TAG}
 docker push ${REPOSITORY_URI}:${IMAGE_TAG}

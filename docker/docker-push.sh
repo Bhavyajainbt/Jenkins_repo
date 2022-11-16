4558cd541d30517368b5fbfed98536440c8dc0d6
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-16 15:20:12 +0530

    Update docker-push.sh

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -10,3 +10,5 @@ else
 fi
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f5k7w3r3
+docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG
+docker push ${IMAGE_REPO_NAME}:${IMAGE_TAG}

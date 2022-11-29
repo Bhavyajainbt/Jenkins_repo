d1651812d492011a3a32a8ab1221faadc153af35
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-29 14:21:15 +0530

    Update terraform-deploy.sh

--- terraform/terraform-deploy.sh
+++ terraform/terraform-deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -ne 1 ];then
+  echo "Please select environment in which the resource has to be deployed."
+  exit 0;
+env_dir=$1
+
 cd terraform/
 terraform init
+cd ${env_dir}
 terraform apply -auto-approve

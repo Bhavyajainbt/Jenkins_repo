e576a932dc6b348ed72334d3886aec4d433ac658
Bhavyajainbt <47173798+Bhavyajainbt@users.noreply.github.com>
2022-11-29 14:34:47 +0530

    Update terraform-deploy.sh

--- terraform/terraform-deploy.sh
+++ terraform/terraform-deploy.sh
@@ -7,6 +7,9 @@ fi
 env_dir=$1
 
 cd terraform/
-terraform init
+echo "Moved to terraform remote directory"
 cd ${env_dir}
+echo "Moved to sub environment directory"
+terraform init
+echo "terraform init is successful"
 terraform apply -auto-approve

093c403302e38f07b8e1ccd134ad366707256b78
unknown <bhavyajainbt@gmail.com>
2022-12-01 21:36:50 +0530

    Test code

--- terraform/dev/aws-vpc.tf
+++ terraform/dev/aws-vpc.tf
@@ -1,7 +1,17 @@
+variable "vpc_cidr_block" {
+  type = string
+  default = "172.50.0.0/16"
+}
+
+variable "vpc-resource-name" {
+  type = string
+}
+
 resource "aws_vpc" "node-dev-vpc" {
-    cidr_block = "172.17.17.0/24"
-    enable_dns_support = true
-    tags = {
-      "name" = "node-dev-vpc"
-    }
+  #cidr_block = "172.17.17.0/24"
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  tags = {
+    "name" = "node-dev-vpc"
+  }
 }

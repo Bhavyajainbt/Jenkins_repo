bf474799a75c5d4d49c0b1cab2ec91313a29c92c
unknown <bhavyajainbt@gmail.com>
2022-12-01 21:48:11 +0530

    Updation

--- terraform/dev/aws-vpc.tf
+++ terraform/dev/aws-vpc.tf
@@ -2,10 +2,6 @@ variable "vpc_cidr_block" {
   type = string
 }
 
-variable "vpc-resource-name" {
-  type = string
-}
-
 resource "aws_vpc" "node-dev-vpc" {
   #cidr_block = "172.17.17.0/24"
   cidr_block = var.vpc_cidr_block

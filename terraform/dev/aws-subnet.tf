06f45239b0975a80b1869267e21d0d57965808e5
unknown <bhavyajainbt@gmail.com>
2022-12-01 22:09:33 +0530

    subnet cidr updation

--- terraform/dev/aws-subnet.tf
+++ terraform/dev/aws-subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "main-dev-subnet" {
     vpc_id = aws_vpc.node-dev-vpc.id
-    cidr_block = "172.50.40.0/24"
+    cidr_block = "172.10.0.44/24"
 
     tags = {
       "name" = "main-dev-subnet"
